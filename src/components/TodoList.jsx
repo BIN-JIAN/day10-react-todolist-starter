e437fb0d81998f76914a6bb4b20fa2a7d735987b
BIN <BIN.JIAN@OOCL.COM>
2025-09-12 15:06:48 +0800

    feat: add todolist

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,17 @@
+import {useContext} from "react";
+import {TodoContext} from "../contexts/TodoContext";
+import'./TodoList.css'
 const TodoList = () => {
-  return (
-      <div>This is the TodoList Component.</div>
-  );
+
+  const {state, dispatch} = useContext(TodoContext);
+  return <div className="todo-group">
+    <div>This is the TodoList Component.</div>
+    {
+      state.map(todo => {
+        return <div className={'todo-item'}>{todo.text}</div>
+      })
+    }
+  </div>
 }
 
 export default TodoList
