b638bf3d96e4614251723ed1e72faa2ce3e6f8d5
BIN <BIN.JIAN@OOCL.COM>
2025-09-15 11:09:07 +0800

    feat: add router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import { useReducer } from "react";
 import './App.css';
-import TodoList from "./components/TodoList";
-import { initialState, todoReducer } from "./reducers/todoReducer";
-import { TodoContext } from "./contexts/TodoContext";
+import {
+  createBrowserRouter,
+  NavLink,
+  Outlet,
+  RouterProvider
+} from "react-router";
+
+function DefaultLayout() {
+  return<>
+    <header>
+      <nav>
+        <ul>
+          <li><NavLink to={'/'}>Home</NavLink></li>
+          <li>Todo List</li>
+          <li><NavLink to={'/about'}>About</NavLink></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <h1>xxx</h1>
+      <Outlet></Outlet>
+    </main>
+    <footer>footer copyright</footer>
+  </>;
+}
+
+const routes=[
+  {
+    path: '/',
+    element: <DefaultLayout/>,
+    children: [{
+      index: true,         // 修改为 index: true，而不是 path: '/'
+      element: <h1>Home page</h1>,
+    },{
+      path:'about',
+      element: <h1>About page</h1>
+    } ]
+  }
+]
+const router = createBrowserRouter(routes);
 
 function App() {
-  // the Hooks API manage component data state
-  const [state, dispatch] = useReducer(todoReducer, initialState);
-
   return (
     <div className="App">
-      <TodoContext.Provider value={{state, dispatch}}>
-        <TodoList/>
-      </TodoContext.Provider>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
